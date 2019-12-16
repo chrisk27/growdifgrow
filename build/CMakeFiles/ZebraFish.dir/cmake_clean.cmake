82a2b945716cfe8acf6c8414922ab54c4e0dfaa3
Chris K <chriskonow27@gmail.com>
2019-12-16 18:16:17 -0500

    Added CMake files, builds but segementation fault

---
+++ build/CMakeFiles/ZebraFish.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ZebraFish.dir/main.cpp.o"
+  "ZebraFish.pdb"
+  "ZebraFish"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ZebraFish.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
